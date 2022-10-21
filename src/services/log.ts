40a6355742aff6a2365f48216a51d11709497479
impuLssse <durasellsx@gmail.com>
2022-10-21 22:00:54 +0300

    first load

---
+++ src/services/log.ts
@@ -0,0 +1,14 @@
+
+
+export class Logger {
+
+    /*
+        Метод log нужен чтобы вызывать его и передать в него массив/переменную
+    */
+
+    public info ( data: Array<string | number> ) {   
+        data.forEach( (item) => {
+            console.log(item)
+        })
+    }
+}
